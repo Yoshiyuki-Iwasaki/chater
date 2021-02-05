45e6ce30baf651f306744dfa698601432d6adb8e
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-02-06 01:48:07 +0900

    [fix] mainページ レイアウト修正

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import SignIn from './SignIn.js'
-import Main from './Main.js'
+import SignIn from "./SignIn.js";
+import Main from "./Main.js";
+import config from "../config.json";
 
 export default () => {
-  const [name, setName] = useState('');
-  console.log({ name });
+  const [name, setName] = useState("");
 
-  if (name === '') {
+  if (name === "" && config.signInEnabled === true) {
     return <SignIn setName={setName} />;
   } else {
     return <Main name={name} />;
