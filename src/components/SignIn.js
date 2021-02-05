e7558980844480e311c72b6e51d65b5f0f179ea2
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-02-06 05:34:28 +0900

    [add] MessageField追加

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,18 +1,22 @@
-import React, {useEffect, useState} from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React, { useEffect, useState } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 function Copyright(props) {
   return (
     <Typography variant="body2" color="textSecondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://twitter.com/_iwasakiii"
-      target="_blank" rel="noopener">
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="https://twitter.com/_iwasakiii"
+        target="_blank"
+        rel="noopener"
+      >
         Iwasakiii
       </Link>
     </Typography>
@@ -21,16 +25,15 @@ function Copyright(props) {
 
 export default function SignIn({ setName }) {
   const [disabled, setDisabled] = useState(true);
-  const [string, setString] = useState('');
-  const [isComposed, setIsComposed] = useState('false');
+  const [string, setString] = useState("");
+  const [isComposed, setIsComposed] = useState(false);
   // console.log({ disabled, string, isComposed });
 
   // stringが変化したタイミングで発火,文字が入力された時のみdisabledを削除する
-  useEffect(
-    () => {
-      const disabled = string === '';
-      setDisabled(disabled);
-    }, [string]);
+  useEffect(() => {
+    const disabled = string === "";
+    setDisabled(disabled);
+  }, [string]);
 
   return (
     <Container component="main" maxWidth="xs">
@@ -38,9 +41,9 @@ export default function SignIn({ setName }) {
       <Box
         sx={{
           marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
         <Typography component="h1" variant="h5">
@@ -50,7 +53,7 @@ export default function SignIn({ setName }) {
           component="form"
           noValidate
           sx={{
-            width: '100%', // Fix IE11 issue.
+            width: "100%", // Fix IE11 issue.
             mt: 1,
           }}
         >
@@ -62,18 +65,18 @@ export default function SignIn({ setName }) {
             label="ニックネーム"
             name="name"
             autoFocus
-            onChange={(e) => setString(e.target.value)}
-            onKeyDown={(e) => {
+            onChange={e => setString(e.target.value)}
+            onKeyDown={e => {
               if (isComposed) return; //isComposedがtrueの時にアーリーリターン
 
-              if (e.key === 'Enter') {
+              if (e.key === "Enter") {
                 // console.log({ key: e.key })
-                setName(e.target.value) // エンターを押したらnameを更新する
+                setName(e.target.value); // エンターを押したらnameを更新する
                 e.preventDefault();
               }
             }}
             onCompositionStart={() => setIsComposed(true)}
-            onCompositionEnd = {() => setIsComposed(false)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             disabled={disabled} // ボタンの初期値をdisabledにする
@@ -82,7 +85,7 @@ export default function SignIn({ setName }) {
             variant="contained"
             sx={{ mt: 3, mb: 2 }}
             onClick={() => {
-              setName({string}) // ボタンを押したらnameを更新する
+              setName({ string }); // ボタンを押したらnameを更新する
             }}
           >
             はじめる
