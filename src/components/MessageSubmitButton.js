38515f7a3fea37332cee0f0decf0560c458b4bce
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-02-06 20:02:59 +0900

    [fix] テキスト入力時のフォーカス修正

--- src/components/MessageSubmitButton.js
+++ src/components/MessageSubmitButton.js
@@ -3,7 +3,7 @@ import IconButton from "@material-ui/core/IconButton";
 import SendIcon from "@material-ui/icons/Send";
 import { pushMessage } from "../firebase";
 
-const MessageSubmitButton = ({ name, setText, text }) => {
+const MessageSubmitButton = ({ inputEl, name, setText, text }) => {
   return (
     <IconButton
       aria-label="delete"
@@ -11,6 +11,7 @@ const MessageSubmitButton = ({ name, setText, text }) => {
       onClick={() => {
         pushMessage({ name, text });
         setText("");
+        inputEl.current.focus();
       }}
     >
       <SendIcon />
